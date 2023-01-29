be3edc4263bd88f65c6f6ca9e7e1b8e32e82ee2a
rahuldotrnair <rahuldotrnair@gmail.com>
2023-01-29 21:54:27 +0530

    Update contact form

--- js/main.js
+++ js/main.js
@@ -94,3 +94,53 @@
     });
 })(jQuery);
 
+function sendContact() {
+	var valid;	
+    console.log("helooooooooooo")
+	valid = validateContact();
+	if(valid) {
+		jQuery.ajax({
+		url: "contact_mail.php",
+		data:'userName='+$("#userName").val()+'&userEmail='+$("#userEmail").val()+'&subject='+$("#subject").val()+'&content='+$(content).val(),
+		type: "POST",
+		success:function(data){
+		$("#mail-status").html(data);
+		},
+		error:function (){}
+		});
+	}
+}
+
+function validateContact() {
+	var valid = true;	
+	$(".demoInputBox").css('background-color','');
+	$(".info").html('');
+	
+	if(!$("#userName").val()) {
+		$("#userName-info").html("(required)");
+		$("#userName").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#userEmail").val()) {
+		$("#userEmail-info").html("(required)");
+		$("#userEmail").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+		$("#userEmail-info").html("(invalid)");
+		$("#userEmail").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#subject").val()) {
+		$("#subject-info").html("(required)");
+		$("#subject").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#content").val()) {
+		$("#content-info").html("(required)");
+		$("#content").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	
+	return valid;
+}
