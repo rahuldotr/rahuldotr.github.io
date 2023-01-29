9873b57e79f281f38170583807befa0ae0888b4c
rahuldotrnair <rahuldotrnair@gmail.com>
2023-01-29 21:33:29 +0530

    Update content

--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@
         var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output', {
             strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
+            typeSpeed: 50,
+            backSpeed: 50,
             smartBackspace: false,
             loop: true
         });
