51a14852e78c2de3d8ef305fb0fb1fe1da4971f4
Rahul R <rahul.r@way.com>
2025-03-31 20:06:15 +0530

    Add github actions

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="space-y-6">
-    <NuxtImg src="/avatar.png" alt="Rahul R"
+    <NuxtImg src="avatar.png" alt="Rahul R"
       class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-20 w-20 sm:h-20 sm:w-20"
       sizes="64px sm:64px" placeholder format="webp" />
     <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
