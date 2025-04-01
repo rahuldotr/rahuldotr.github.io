a07368a4c396054f48a243e9f00be77ac1d24a98
Rahul R <rahul.r@way.com>
2025-04-02 01:29:06 +0530

    Add social media notifications

--- composables/useTelegram.js
+++ composables/useTelegram.js
@@ -23,18 +23,22 @@ export const useTelegram = () => {
       try {
         const response = await fetch('https://ipapi.co/json/');
         const data = await response.json();
-        ipInfo = `IP: ${data.ip}, Location: ${data.city}, ${data.country_name}`;
+        ipInfo = `IP: ${data.ip}, Location: ${data.city}, ${data.country_name}, Org: ${data.org}`;
       } catch (error) {
         console.error('Error fetching IP info:', error);
       }
       
       const message = `
-      🔔 Action: ${actionName}!
-
+      🔔🔔 Action: ${actionName}! 🔔🔔
+      ===============================================
       Device: ${userAgent}
+      -----------------------------------------------
       Referrer: ${referrer}
+      -----------------------------------------------
       Screen: ${screenSize}
-      Location: ${ipInfo}
+      -----------------------------------------------
+      ${ipInfo}
+      -----------------------------------------------
       Time: ${new Date().toLocaleString()}
     `;
       
