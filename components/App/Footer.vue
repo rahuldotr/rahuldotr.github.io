ed1cc10e52f4643092539bffb7a1d2b4db36c52b
Rahul R <rahuldotrnair@gmail.com>
2025-04-25 19:02:38 +0530

    Removed the visit counter section

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -3,12 +3,12 @@
     class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-12"
   >
     <div class="flex flex-col items-center justify-center mb-4">
-      <VisitorCounter />
+      <!-- <VisitorCounter /> -->
     </div>
     <p>© 2025 Rahul R. All rights reserved.</p>
   </footer>
 </template>
 
 <script setup>
-import VisitorCounter from '~/components/App/VisitorCounter.vue';
+// import VisitorCounter from '~/components/App/VisitorCounter.vue';
 </script>
